1cea2cd7012bb9aed4543bc1cc601ceb6e33083a
SadiQ HabiL <kingqabil@live.com>
2022-05-31 16:54:35 +0300

    Create NavBar Component and Add NavLinks

---
+++ src/components/NavBar.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+const NavPanel = () => {
+  const links = [
+    {
+      id: 1,
+      path: '/',
+      text: 'Home',
+    },
+    {
+      id: 2,
+      path: '/reservations',
+      text: 'My Reservations',
+    },
+  ];
+
+  return (
+    <section>
+      <nav>
+        <ul>
+          {links.map((link) => (
+            <li key={link.id}>
+              <NavLink
+                to={link.path}
+                exact
+              >
+                {link.text}
+              </NavLink>
+            </li>
+          ))}
+        </ul>
+      </nav>
+    </section>
+  );
+};
+
+export default NavPanel;
