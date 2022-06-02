50a0e0b5f3f125f117297cf0ee21f0243e02081e
SadiQ HabiL <kingqabil@live.com>
2022-06-02 22:22:41 +0300

    auth pages styling

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,16 +1,14 @@
 import { applyMiddleware, combineReducers, createStore } from 'redux';
+import logger from 'redux-logger';
 import thunk from 'redux-thunk';
 import { persistReducer, persistStore } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
-import { roomsReducer } from './rooms/rooms';
+// eslint-disable-next-line import/no-named-as-default
+import roomsReducer from './rooms/rooms';
 import authReducer from './auth';
-import { reservationsReducer } from './reservations/reservations';
-import { setIdReducer } from './reservations/setId';
 
 const reducer = combineReducers({
   roomsReducer,
-  reservationsReducer,
-  setIdReducer,
   auth: authReducer,
 });
 
@@ -20,7 +18,7 @@ const persistConfig = {
 };
 const persistedReducer = persistReducer(persistConfig, reducer);
 
-const store = createStore(persistedReducer, applyMiddleware(thunk));
+const store = createStore(persistedReducer, applyMiddleware(thunk, logger));
 export const persistor = persistStore(store);
 
 export default store;
