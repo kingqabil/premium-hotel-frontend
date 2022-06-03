9537b5cd7701e0ca5fe6dd98645787e554a60067
Hamza <hamzatarar8@gmail.com>
2022-06-03 19:12:43 +0200

    Completed my reservations page.

--- src/pages/MyReservations.js
+++ src/pages/MyReservations.js
@@ -1,7 +1,23 @@
 import React from 'react';
+import phfrom from '../images/ph.png';
+import NavBar from '../components/NavBar';
 
 function MyReservation() {
-  return (<h1>My MyReservation</h1>);
+  return (
+    <div className="home">
+      <div className="nav">
+        <img src={ph} className="ph-logo" alt="" />
+        <NavBar />
+      </div>
+      <div className="main">
+        <h1>My reservations</h1>
+        <h2>
+          You are able to cancel the reservation before 24 hours of the
+          reservation date
+        </h2>
+      </div>
+    </div>
+  );
 }
 
 export default MyReservation;
