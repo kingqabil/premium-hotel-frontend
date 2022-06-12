ba313a57af088dc15ecd7cbc70844a36b128ea04
SadiQ HabiL <kingqabil@live.com>
2022-06-12 04:15:04 +0300

    Update MyReservations.js

--- src/pages/MyReservations.js
+++ src/pages/MyReservations.js
@@ -51,7 +51,7 @@ function MyReservation() {
         )}
         ;
         <div className="reservations">
-          {reservations && reservations.map((reservation) => (
+          {Array.isArray(reservations) && reservations.map((reservation) => (
             <Reservation reservation={reservation} key={reservation.uid} />
           ))}
         </div>
