e7920cdf9996f3d3387fd7288ac3e182ba873e71
Bertrand Mutangana Ishimwe <mutanganabertrand@gmail.com>
2022-06-22 11:13:27 +0200

    Write tests for the reservation form page

---
+++ src/__tests__/pages/addReservation.test.js
@@ -0,0 +1,32 @@
+import renderer from "react-test-renderer";
+import "@testing-library/jest-dom";
+import { screen, render } from "@testing-library/react";
+import { Provider } from "react-redux";
+import { BrowserRouter as Router } from "react-router-dom";
+import AddReservation from "../../pages/AddReservation";
+import store from "../../redux/configureStore";
+
+const AddReservationProvider = () => (
+  <Provider store={store}>
+    <Router>
+      <AddReservation />
+    </Router>
+  </Provider>
+);
+
+describe("render component", () => {
+  it("renders the rooms list form", () => {
+    render(<AddReservationProvider />);
+    expect(screen.getByLabelText(/check in/i)).toBeInTheDocument();
+    expect(screen.getByLabelText(/check out/i)).toBeInTheDocument();
+    expect(screen.getByLabelText(/room/i)).toBeInTheDocument();
+    expect(screen.getByText(/reserve/i)).toBeInTheDocument();
+  });
+});
+
+describe("Snapshot test", () => {
+  it("looks the same as the snapshot", () => {
+    const component = renderer.create(<AddReservationProvider />).toJSON();
+    expect(component).toMatchSnapshot();
+  });
+});
